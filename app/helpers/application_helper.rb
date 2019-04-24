aab28f225c3e75483f80d937cbf5dcd4d9c0345c
Lucas Arthur Penz <lucasarthurpenz@gmail.com>
2019-04-24 18:26:07 -0300

    Aula dia 10 de abril 2019

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def menu(description, path)
+    content_tag(:li) do
+      link_to description, path
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  def resumePost(text)
+    # text[0..300] + "..."
+    truncate(text, length: 300)
+  end
 end
